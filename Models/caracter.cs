bdc30f583e0a75f41a698a98c5eb45c55de8969e
ghaliafawzi <ghaliafawzi18@gmail.com>
2023-07-10 13:18:24 +0300

    initial commit

---
+++ Models/caracter.cs
@@ -0,0 +1,15 @@
+namespace dotnet_rpg.Models
+{
+    public class caracter
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frdo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defence { get; set; } = 10;
+        public int intelligence { get; set; } = 10;
+
+
+    }
+}
